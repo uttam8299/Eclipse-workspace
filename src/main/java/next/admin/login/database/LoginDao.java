90d5c79514da056bc1705bda6559051635f1ffc8
uttam <uttam@Uttam>
2022-02-18 15:09:33 +0530

    first commit

---
+++ src/main/java/next/admin/login/database/LoginDao.java
@@ -0,0 +1,69 @@
+package next.admin.login.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import next.admin.login.bean.LoginBean;
+
+public class LoginDao {
+	
+	private String dbUrl = "jdbc:mysql://localhost:3306/userdb";
+	private String dbUname = "root";
+	private String dbPassword = "root";
+	private String dbDriver = "com.mysql.cj.jdbc.Driver";
+	
+	public void loadDriver(String dbDriver)
+	{
+		try {
+			Class.forName(dbDriver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection() throws SQLException
+	{
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(dbUrl, dbUname, dbPassword);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public boolean validate(LoginBean loginBean)
+	{
+		boolean status = false;
+		
+		loadDriver(dbDriver);
+		Connection con = null;
+		try {
+			con = getConnection();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("error in connecting db");
+			e1.printStackTrace();
+		}
+		
+		String sql = "select * from login where username = ? and password =?";
+		PreparedStatement ps;
+		try {
+		ps = con.prepareStatement(sql);
+		ps.setString(1, loginBean.getUsername());
+		ps.setString(2, loginBean.getPassword());
+		ResultSet rs = ps.executeQuery();
+		status = rs.next();
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return status;
+	}
+	}
---
+++ src/main/java/next/admin/login/web/LoginServlet.java
@@ -0,0 +1,55 @@
+package next.admin.login.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import next.admin.login.bean.LoginBean;
+import next.admin.login.database.LoginDao;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		LoginBean loginBean = new LoginBean();
+		loginBean.setUsername(username);
+		loginBean.setPassword(password);
+		
+		LoginDao loginDao = new LoginDao();
+		if(loginDao.validate(loginBean)) {
+			response.sendRedirect("loginSuccess.jsp");
+		}
+		else {
+			response.sendRedirect("login.jsp");
+		}
+	}
+
+}
